93a4643a5d4f2646093a522ce24df813af36a984
py哥 <290070744@qq.com>
2020-12-18 10:55:59 +0800

    1217

--- src/router/index.js
+++ src/router/index.js
@@ -126,7 +126,7 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-    if (to.path == '/login') return next()
+    if (to.path === '/login') return next()
     const token = window.sessionStorage.getItem('token')
     if (!token) return next('/login')
     next()
